b9858a8d0d1b66e9472ba16c12685a3482c618fa
JangKroed <didlsdydgh@gmail.com>
2022-10-25 07:46:18 +0900

    조회수 테이블 추가

---
+++ migrations/4-create-qna-view.js
@@ -0,0 +1,25 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('qnaViews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      ip: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      time: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('qnaViews');
+  },
+};
