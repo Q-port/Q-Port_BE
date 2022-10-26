9d74f72d474db653bca1cace3965dd5df1029a4a
Monggle88 <fmonggle88@gmail.com>
2022-10-26 20:25:16 +0900

    add answerListByUserId fix answer Codes

--- repositories/answers.repository.js
+++ repositories/answers.repository.js
@@ -1,14 +1,11 @@
 const { Question, Answer, User } = require('../models');
 
 class AnswersRepository {
-  constructor() {
-    this.Answer = Answer;
-    this.User = User;
-  }
+  constructor() {}
 
   // 답변 작성
   createAnswer = async (answer) => {
-    await this.Answer.create({
+    await Answer.create({
       ...answer,
       createdAt: Date.now(),
       updatedAt: Date.now(),
@@ -17,35 +14,36 @@ class AnswersRepository {
 
   // QuestionId로 답변 모두 불러오기
   findByQuestionId = async (questionId) => {
-    return await this.Answer.findAll({
+    return await Answer.findAll({
       where: { questionId },
     });
   };
 
   // AnswerId로 답변 불러오기
   findByAnswerId = async (answerId) => {
-    return await this.Answer.findOne({
+    return await Answer.findOne({
       where: { answerId },
     });
   };
 
   // userId로 답변글 불러오기
   findByUserId = async (userId) => {
-    return await this.Answer.findAll({
+    return await Answer.findAll({
+      attributes: ['questionId', 'createdAt', 'answerId'],
       where: { userId },
     });
   };
 
   // 답변 중복확인
   findByDuplicate = async (questionId, userId) => {
-    return await this.Answer.findOne({
+    return await Answer.findOne({
       where: { questionId, userId },
     });
   };
 
   // 답변 수정
   updateAnswer = async (answerId, content, imgUrl) => {
-    await this.Answer.update(
+    await Answer.update(
       { content, imgUrl, updatedAt: Date.now() },
       { where: { answerId } }
     );
@@ -53,7 +51,7 @@ class AnswersRepository {
 
   // 답변 삭제
   deleteAnswer = async (answerId) => {
-    await this.Answer.destroy({
+    await Answer.destroy({
       where: {
         answerId,
       },
@@ -71,23 +69,17 @@ class AnswersRepository {
 
   // questionId에 따른 답변 갯수 카운트
   answerCountByQuestionId = async (questionId) => {
-    return await Answer.count({ where: { questionId: questionId } });
+    return await Answer.count({
+      where: { questionId: questionId },
+    });
   };
 
-  // 이미지 업로드
-  updateImage = async (answerId, imgUrl) => {
-    try {
-      await this.Answer.update(
-        {
-          imgUrl,
-        },
-        {
-          where: { answerId },
-        }
-      );
-    } catch (error) {
-      throw new Error(`UnhandleMysqlSequelizeError: ${error}`);
-    }
+  // 질문 글 상태 확인용 question 불러오기
+  getQuestionByQuestionId = async (questionId) => {
+    return await Question.findOne({
+      attributes: ['questionId', 'title', 'selectedAnswer'],
+      where: { questionId },
+    });
   };
 }
 
--- services/answers.service.js
+++ services/answers.service.js
@@ -7,15 +7,15 @@ class AnswersService {
 
   // 답변 작성
   createAnswer = async (req, res, next) => {
+    // 기본 데이터 setup
     const { user } = res.locals;
     const { questionId } = req.params;
     const { content } = req.body;
-    const imageFileName = req.file ? req.file.key : null;
 
-    const imgUrl = imageFileName
-      ? process.env.S3_STORAGE_URL + imageFileName
-      : null;
+    // 이미지 처리
+    const imgUrl = this.attachImage(req);
 
+    // 게시할 답변 생성
     const answer = {
       questionId: questionId,
       userId: user.userId,
@@ -31,13 +31,17 @@ class AnswersService {
 
     // 본인 질문에 답변 작성 불가
     const isMyQuestion = await this.questionsRepository.findByQna(questionId);
-    if (isMyQuestion.userId === user.userId)
+    if (isMyQuestion.userId === user.userId) {
+      res.status(403);
       throw new Error('본인의 질문에 답변할 수 없습니다.');
+    }
 
     // 해결된 게시글에 답변 작성 불가
     const isDone = await this.questionsRepository.findByQna(questionId);
-    if (isDone.selectedAnswer > 0)
+    if (isDone.selectedAnswer > 0) {
+      res.status(403);
       throw new Error('해결완료 된 질문에 답변할 수 없습니다.');
+    }
 
     // 중복작성 불가
     // 해당 질문에 이전에 답변한 유저 답변 불가
@@ -45,20 +49,23 @@ class AnswersService {
       questionId,
       user.userId
     );
-    if (isDuplicateUser) throw new Error('답변을 중복해서 작성할 수 없습니다.');
+    if (isDuplicateUser) {
+      res.status(403);
+      throw new Error('답변을 중복해서 작성할 수 없습니다.');
+    }
 
     // 한 질문에 답변 10개 제한
     const answerCount = await this.answersRepository.answerCountByQuestionId(
       questionId
     );
-    if (answerCount === 10)
+    if (answerCount === 10) {
+      res.status(403);
       throw new Error('한 질문에 대한 답변은 10개를 넘을 수 없습니다.');
+    }
 
     // 가공된 질문글 데이터를 repository로 전달
     await this.answersRepository.createAnswer(answer);
     await this.answersRepository.addAnswerCount({ questionId });
-
-    res.status(200);
   };
 
   // 답변 불러오기
@@ -76,19 +83,18 @@ class AnswersService {
     const { content } = req.body;
     const { user } = res.locals;
 
-    const imageFileName = req.file ? req.file.key : undefined;
-
-    const imgUrl = imageFileName
-      ? process.env.S3_STORAGE_URL + imageFileName
-      : undefined;
+    // 이미지 처리
+    const imgUrl = this.attachImage(req);
 
     // 내용 유효성 검사
     if (!content.trim()) throw new Error('내용을 입력해주세요.');
 
     // 본인의 게시글인지 확인
     const answer = await this.answersRepository.findByAnswerId(answerId);
-    if (answer.userId !== user.userId)
+    if (answer.userId !== user.userId) {
+      res.status(403);
       throw new Error('본인만 수정할 수 있습니다.');
+    }
     if (!answer) throw new Error('잘못된 요청입니다.');
 
     await this.answersRepository.updateAnswer(answerId, content, imgUrl);
@@ -102,8 +108,10 @@ class AnswersService {
     const { answerId } = req.params;
     const answer = await this.answersRepository.findByAnswerId(answerId);
 
-    if (answer.userId !== user.userId)
+    if (answer.userId !== user.userId) {
+      res.status(403);
       throw new Error('본인만 삭제할 수 있습니다.');
+    }
     if (!answer) throw new Error('잘못된 요청입니다.');
 
     await this.answersRepository.deleteAnswer(answerId);
@@ -111,39 +119,50 @@ class AnswersService {
     await this.answersRepository.subtractAnswerCount({ questionId });
   };
 
-  // 이미지
-  updateImage = async (req, res) => {
-    const { userId } = res.locals.user;
-    const { answerId } = req.params;
-    const imageFileName = req.file ? req.file.key : null;
-
-    if (!imageFileName) throw new Error('게시물 이미지가 빈 값');
-    const findByWriter = await this.answersRepository.findByAnswerId(answerId);
-
-    if (findByWriter.userId !== userId)
-      throw new Error('본인만 수정할 수 있습니다.');
-    if (!findByWriter) throw new Error('잘못된 요청입니다.');
-
-    await this.answersRepository.updateImage(
-      answerId,
-      process.env.S3_STORAGE_URL + imageFileName
-    );
-  };
-
-  // userId기준 답변 목록 불러오기
+  // userId기준 답변을 한 질문 목록 불러오기 + 상태
   getAnswersByUserId = async (req, res) => {
     const { userId } = req.params;
-    const answers = this.answersRepository.findByUserId(userId);
+    const foundAnswer = await this.answersRepository.findByUserId(userId);
 
-    for (const first in answers) {
-      for (const key in first) {
-        if (user.hasOwnProperty(key)) {
-          console.log(`${key}: ${user[key]}`);
-        }
+    let answers = new Object();
+
+    for (let i = 0; i < foundAnswer.length; i++) {
+      answers[i] = foundAnswer[i];
+
+      let questionId = foundAnswer[i].questionId;
+
+      let tempQuestion = await this.answersRepository.getQuestionByQuestionId(
+        questionId
+      );
+
+      answers[i]['dataValues'].title = tempQuestion.title;
+
+      console.log(tempQuestion.selectedAnswer);
+
+      if (tempQuestion.selectedAnswer === 0) {
+        answers[i]['dataValues'].status = '진행중';
+      } else if (tempQuestion.selectedAnswer !== foundAnswer[i].answerId) {
+        answers[i]['dataValues'].status = '완료됨';
+      } else if (tempQuestion.selectedAnswer === foundAnswer[i].answerId) {
+        answers[i]['dataValues'].status = '채택됨';
       }
     }
 
     return answers;
+  };
+
+  // 반복되는 코드
+
+  // 이미지 처리
+  attachImage = async (req) => {
+    // 이미지 첨부 여부 확인
+    const imageFileName = req.file ? req.file.key : null;
+    // 이미지 Url 생성
+    const imgUrl = imageFileName
+      ? process.env.S3_STORAGE_URL + imageFileName
+      : null;
+
+    return imgUrl;
   };
 }
 
