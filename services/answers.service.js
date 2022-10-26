ed012f6eef1c5b18c0e3e753abbacace522b63de
Monggle88 <fmonggle88@gmail.com>
2022-10-26 21:25:54 +0900

    Temporary save

--- services/answers.service.js
+++ services/answers.service.js
@@ -7,10 +7,12 @@ class AnswersService {
 
   // 답변 작성
   createAnswer = async (req, res, next) => {
-    // 기본 데이터 setup
     const { user } = res.locals;
     const { questionId } = req.params;
     const { content } = req.body;
+
+    // 데이터 유효성 검사
+    this.ValidationOfWritingAnswers(user, content, questionId);
 
     // 이미지 처리
     const imgUrl = this.attachImage(req);
@@ -27,44 +29,9 @@ class AnswersService {
       updatedAt: Date.now(),
     };
 
-    if (!content.trim()) throw new Error('내용을 입력해주세요.');
-
-    // 본인 질문에 답변 작성 불가
-    const isMyQuestion = await this.questionsRepository.findByQna(questionId);
-    if (isMyQuestion.userId === user.userId) {
-      res.status(403);
-      throw new Error('본인의 질문에 답변할 수 없습니다.');
-    }
-
-    // 해결된 게시글에 답변 작성 불가
-    const isDone = await this.questionsRepository.findByQna(questionId);
-    if (isDone.selectedAnswer > 0) {
-      res.status(403);
-      throw new Error('해결완료 된 질문에 답변할 수 없습니다.');
-    }
-
-    // 중복작성 불가
-    // 해당 질문에 이전에 답변한 유저 답변 불가
-    const isDuplicateUser = await this.answersRepository.findByDuplicate(
-      questionId,
-      user.userId
-    );
-    if (isDuplicateUser) {
-      res.status(403);
-      throw new Error('답변을 중복해서 작성할 수 없습니다.');
-    }
-
-    // 한 질문에 답변 10개 제한
-    const answerCount = await this.answersRepository.answerCountByQuestionId(
-      questionId
-    );
-    if (answerCount === 10) {
-      res.status(403);
-      throw new Error('한 질문에 대한 답변은 10개를 넘을 수 없습니다.');
-    }
-
     // 가공된 질문글 데이터를 repository로 전달
     await this.answersRepository.createAnswer(answer);
+    // 답변갯수 + 1
     await this.answersRepository.addAnswerCount({ questionId });
   };
 
@@ -115,6 +82,7 @@ class AnswersService {
     if (!answer) throw new Error('잘못된 요청입니다.');
 
     await this.answersRepository.deleteAnswer(answerId);
+    // 답변 갯수 - 1
     const questionId = answer.questionId;
     await this.answersRepository.subtractAnswerCount({ questionId });
   };
@@ -122,36 +90,37 @@ class AnswersService {
   // userId기준 답변을 한 질문 목록 불러오기 + 상태
   getAnswersByUserId = async (req, res) => {
     const { userId } = req.params;
-    const foundAnswer = await this.answersRepository.findByUserId(userId);
+    // 해당 유저가 작성한 답변글 가져오기 attributes: ['questionId', 'createdAt', 'answerId']
+    const foundAnswers = await this.answersRepository.findByUserId(userId);
 
+    // 반환 예정인 answer 선언
     let answers = new Object();
 
-    for (let i = 0; i < foundAnswer.length; i++) {
-      answers[i] = foundAnswer[i];
+    // foundAnswers 내부를 순회하며 데이터 가공
+    for (let i = 0; i < foundAnswers.length; i++) {
+      // attributes 처리된 데이터를 answers에 삽입
+      answers[i] = foundAnswers[i];
 
-      let questionId = foundAnswer[i].questionId;
-
+      // 유저가 작성한 답변의 질문 가져오기 attributes: ['questionId', 'title', 'selectedAnswer']
       let tempQuestion = await this.answersRepository.getQuestionByQuestionId(
-        questionId
+        foundAnswers[i].questionId
       );
 
+      // 가져온 답변의 제목을 answers에 삽입
       answers[i]['dataValues'].title = tempQuestion.title;
 
-      console.log(tempQuestion.selectedAnswer);
-
+      // 질문의 진행상황 및 해당유저의 채택여부 식별하여 answers에 삽입
       if (tempQuestion.selectedAnswer === 0) {
         answers[i]['dataValues'].status = '진행중';
-      } else if (tempQuestion.selectedAnswer !== foundAnswer[i].answerId) {
+      } else if (tempQuestion.selectedAnswer !== foundAnswers[i].answerId) {
         answers[i]['dataValues'].status = '완료됨';
-      } else if (tempQuestion.selectedAnswer === foundAnswer[i].answerId) {
+      } else if (tempQuestion.selectedAnswer === foundAnswers[i].answerId) {
         answers[i]['dataValues'].status = '채택됨';
       }
     }
 
     return answers;
   };
-
-  // 반복되는 코드
 
   // 이미지 처리
   attachImage = async (req) => {
@@ -163,6 +132,44 @@ class AnswersService {
       : null;
 
     return imgUrl;
+  };
+
+  // 답변 작성 유효성 검사
+  ValidationOfWritingAnswers = async (user, content, questionId) => {
+    if (!content.trim()) throw new Error('내용을 입력해주세요.');
+
+    // 본인 질문에 답변 작성 불가
+    const isMyQuestion = await this.questionsRepository.findByQna(questionId);
+    if (isMyQuestion.userId === user.userId) {
+      res.status(403);
+      throw new Error('본인의 질문에 답변할 수 없습니다.');
+    }
+
+    // 해결된 게시글에 답변 작성 불가
+    const isDone = await this.questionsRepository.findByQna(questionId);
+    if (isDone.selectedAnswer > 0) {
+      res.status(403);
+      throw new Error('해결완료 된 질문에 답변할 수 없습니다.');
+    }
+
+    // 해당 질문에 이전에 답변한 유저는 중복 답변 불가
+    const isDuplicateUser = await this.answersRepository.findByDuplicate(
+      questionId,
+      user.userId
+    );
+    if (isDuplicateUser) {
+      res.status(403);
+      throw new Error('답변을 중복해서 작성할 수 없습니다.');
+    }
+
+    // 한 질문에 답변 10개 제한
+    const answerCount = await this.answersRepository.answerCountByQuestionId(
+      questionId
+    );
+    if (answerCount === 10) {
+      res.status(403);
+      throw new Error('한 질문에 대한 답변은 10개를 넘을 수 없습니다.');
+    }
   };
 }
 
